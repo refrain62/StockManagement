eab9881395de2c4fa129f5af71c4090391bfcebf
refrain62 <nao_h13@hotmail.com>
2024-03-19 00:12:22 +0900

    値オブジェクト (Value Object)

---
+++ src/Domain/models/Book/Title/Title.ts
@@ -0,0 +1,18 @@
+import { ValueObject } from "../BookId/shared/ValueObject";
+
+type TitleValue = string
+
+export class Title extends ValueObject<TitleValue, 'Title'> {
+  static readonly MAX_LENGTH = 1000
+  static readonly MIN_LNEGTH = 1
+
+  constructor(value: TitleValue) {
+    super(value)
+  }
+
+  protected validate(value: string): void {
+    if (value.length < Title.MIN_LNEGTH || value.length > Title.MAX_LENGTH) {
+      throw new Error(`タイトルは${Title.MIN_LNEGTH}文字以上、${Title.MAX_LENGTH}文字以下でなければなりません。`)
+    }
+  }
+}
